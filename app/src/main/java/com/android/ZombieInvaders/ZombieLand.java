90f2f20e3ab63da5ff46aba156132b37d32faf00
MustafaSabur <musy.sabur@gmail.com>
2015-03-26 14:20:44 +0100

    "adding Zombieland class"

---
+++ app/src/main/java/com/android/ZombieInvaders/ZombieLand.java
@@ -0,0 +1,25 @@
+package com.android.ZombieInvaders;
+
+/**
+ * Created by Sabur on 26/03/15.
+ */
+public class ZombieLand {
+
+
+
+    public ZombieLand(){
+
+    }
+
+    public void createMapTiles(int level){
+
+        switch (level){
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+
+    }
+
+}
